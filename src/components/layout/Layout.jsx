4eb90b136d3e6472c0a1735743bab1498b3682fa
om shinde <Shindeom052@gmail.com>
2026-02-25 20:33:55 +0530

    Added Dashboard page

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,27 +1,35 @@
 import { useState } from "react";
-import { Menu } from "lucide-react";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
-export default function Layout({ children, onLogout }) {
+export default function Layout({
+    children,
+    search,
+    setSearch,
+    selectedDistrict,
+    setSelectedDistrict,
+    connected,
+    onLogout
+}) {
     const [sidebarOpen, setSidebarOpen] = useState(true);
 
     return (
         <div className="flex h-screen bg-slate-100">
 
-            {/* LEFT SIDEBAR */}
             <Sidebar open={sidebarOpen} />
 
-            {/* MAIN AREA */}
             <div className="flex-1 flex flex-col">
 
-                {/* HEADER */}
                 <Header
                     toggleSidebar={() => setSidebarOpen(!sidebarOpen)}
+                    search={search}
+                    setSearch={setSearch}
+                    selectedDistrict={selectedDistrict}
+                    setSelectedDistrict={setSelectedDistrict}
+                    connected={connected}
                     onLogout={onLogout}
                 />
 
-                {/* PAGE CONTENT */}
                 <div className="flex-1 overflow-auto p-6">
                     {children}
                 </div>
