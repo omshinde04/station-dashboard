96432091230a7b4c08bd73ac21ac63f8805962e7
om shinde <Shindeom052@gmail.com>
2026-03-04 14:01:41 +0530

    Fix: Interceptor + Excel export stability

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import StationList from "./StationList";
 export default function Sidebar({ stations, stats }) {
     return (
         <div className="flex flex-col gap-6">
-            <StatsGrid stats={stats} />
+
             <StationList stations={stations} />
         </div>
     );
